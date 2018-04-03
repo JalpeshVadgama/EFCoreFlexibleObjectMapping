7ebbd096ebe2461941dc8819f4d2eefdb5c301e2
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2018-04-03 23:06:39 +0530

    Initial commit with blank console application

---
+++ EFCore2Mapping/EFCore2Mapping/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCore2Mapping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
